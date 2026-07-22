3405b4546cb8e21f9b1ba3de1011d918562c0836
apmnt <aapomo@gmail.com>
2026-07-23 01:05:49 +0300

    704 in fp scala

--- problems/704-binary-search.scala
+++ problems/704-binary-search.scala
@@ -6,36 +6,53 @@
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
 
-//> using file helpers.scala
+package p704
 
-import Helpers.pp
+import leetcode.Helpers.pp
+
+// object Solution {
+//   def search(nums: Array[Int], target: Int): Int = {
+//     var left = 0
+//     var right = nums.length - 1
+//     while (left <= right) {
+//       val mid = left + (right - left) / 2
+//       if (nums(mid) == target) return mid
+//       else if (nums(mid) < target) left = mid + 1
+//       else right = mid - 1
+//     }
+//     -1
+//   }
+// }
 
 object Solution {
-    def search(nums: Array[Int], target: Int): Int = {
-        var left = 0
-        var right = nums.length - 1
-        while (left <= right) {
-            val mid = left + (right - left) / 2
-            if (nums(mid) == target) return mid
-            else if (nums(mid) < target) left = mid + 1
-            else right = mid - 1
-        }
-        -1
+  def search(nums: Array[Int], target: Int): Int = {
+    val left = 0
+    val right = nums.length - 1
+    def go(left: Int, right: Int): Int = {
+      val mid = left + (right - left) / 2
+      if (nums(mid) == target) return mid
+      if (left >= right) return -1
+      if (nums(mid) < target) return go(mid + 1, right)
+      else return go(left, mid - 1)
     }
+    go(left, right)
+  }
 }
 
-@main def run(): Unit = {
-  val nums1 = Array(-1, 0, 3, 5, 9, 12)
-  val target1 = 9
-  println("Example 1")
-  println(s"Input: nums=${pp(nums1)}, target=${pp(target1)}")
-  println(s"Output: ${pp(Solution.search(nums1, target1))}")
-  println(s"Correct: ${pp(4)}")
+object Main {
+  def main(args: Array[String]): Unit = {
+    val nums1 = Array(-1, 0, 3, 5, 9, 12)
+    val target1 = 9
+    println("Example 1")
+    println(s"Input: nums=${pp(nums1)}, target=${pp(target1)}")
+    println(s"Output: ${pp(Solution.search(nums1, target1))}")
+    println(s"Correct: ${pp(4)}")
 
-  val nums2 = Array(-1, 0, 3, 5, 9, 12)
-  val target2 = 2
-  println("Example 2")
-  println(s"Input: nums=${pp(nums2)}, target=${pp(target2)}")
-  println(s"Output: ${pp(Solution.search(nums2, target2))}")
-  println(s"Correct: ${pp(-1)}")
+    val nums2 = Array(-1, 0, 3, 5, 9, 12)
+    val target2 = 2
+    println("Example 2")
+    println(s"Input: nums=${pp(nums2)}, target=${pp(target2)}")
+    println(s"Output: ${pp(Solution.search(nums2, target2))}")
+    println(s"Correct: ${pp(-1)}")
+  }
 }
